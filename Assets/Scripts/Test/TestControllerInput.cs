cc3a0b81f01141f58c6f38bd44b8492b9eaff677
Tom Hemery <tomhemery@outlook.com>
2019-12-12 16:39:17 +0000

    Added VR Support, Began to implement UI (slowly)

---
+++ Assets/Scripts/Test/TestControllerInput.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Valve.VR;
+
+public class TestControllerInput : MonoBehaviour
+{
+
+    public SteamVR_Input_Sources inputSource = SteamVR_Input_Sources.Any;
+    public SteamVR_Action_Boolean grabPinch;
+
+    void OnEnable()
+    {
+        if (grabPinch != null)
+        {
+            grabPinch.AddOnChangeListener(OnTriggerPressed, inputSource);
+        }
+    }
+
+
+
+    private void OnDisable()
+    {
+        if (grabPinch != null)
+        {
+            grabPinch.RemoveOnChangeListener(OnTriggerPressed, inputSource);
+        }
+    }
+
+    private void OnTriggerPressed(SteamVR_Action_Boolean fromAction, SteamVR_Input_Sources fromSource, bool newState)
+    {
+        Debug.Log("Trigger pressed");
+    }
+}
