49b290668d9ebff2b6d03bd0df908f2f364da0df
Tom Hemery <tomhemery@outlook.com>
2020-02-13 13:27:38 +0000

    Fixed a bug caused by disabling the ui while dragging slider, removed bs

---
+++ Assets/Scripts/UI/StopAllSliderInput.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class StopAllSliderInput : MonoBehaviour
+{
+    private Slider[] allSliders;
+    public void Start()
+    {
+        allSliders = GetComponentsInChildren<Slider>();
+    }
+    public void StopAllSliders() {
+        PointerEventData e = new PointerEventData(EventSystem.current);
+        foreach (Slider s in allSliders) {
+            s.OnPointerUp(e);
+        }
+    }
+}
